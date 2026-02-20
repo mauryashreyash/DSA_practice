5cbc24bdd9882f1448ccb485e4ebfe5e8f82c78b
Shreyash Maurya <168304543+shreyash-1908@users.noreply.github.com>
2026-02-20 22:32:15 +0530

    Create 34_FindFirstAndLastPosition.java
    BinarySearch/34_FindFirstAndLastPosition.java

---
+++ Medium/34_FindFirstAndLastPosition.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+    
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int index = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+        
+        return index;
+    }
+    
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int index = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+        
+        return index;
+    }
+}
