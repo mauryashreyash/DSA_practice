b840c2fbc25a9d574deca842d2b67d5e7cdca596
Shreyash Maurya <168304543+shreyash-1908@users.noreply.github.com>
2026-02-22 21:48:59 +0530

    Create 875_KokoEatingBananas.java
    Added binary search on answer solution for LeetCode 875 - Koko Eating Bananas

---
+++ Medium/875_KokoEatingBananas.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        int left = 1;
+        int right = 0;
+        
+        // Find max pile (upper bound)
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (canFinish(piles, h, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
+    private boolean canFinish(int[] piles, int h, int k) {
+        long hours = 0;
+        
+        for (int pile : piles) {
+            hours += (pile + k - 1) / k;  // ceil(pile / k)
+        }
+        
+        return hours <= h;
+    }
+}
