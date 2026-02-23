284ea11dc8043048b57199ccac094759105dbe31
Shreyash Maurya <168304543+shreyash-1908@users.noreply.github.com>
2026-02-23 22:42:10 +0530

    Create 1482_MinimumNumberOfDaysToMakeMBouquets.java
    Added binary search on answer solution for LeetCode 1482 - Minimum Number of Days to Make m Bouquets

---
+++ Medium/1482_MinimumNumberOfDaysToMakeMBouquets.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        
+        long totalNeeded = (long) m * k;
+        if (totalNeeded > bloomDay.length) {
+            return -1;
+        }
+        
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+        
+        for (int day : bloomDay) {
+            left = Math.min(left, day);
+            right = Math.max(right, day);
+        }
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (canMake(bloomDay, m, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
+    private boolean canMake(int[] bloomDay, int m, int k, int days) {
+        int bouquets = 0;
+        int flowers = 0;
+        
+        for (int day : bloomDay) {
+            if (day <= days) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+        
+        return bouquets >= m;
+    }
+}
