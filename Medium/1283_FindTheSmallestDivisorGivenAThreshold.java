592cfaafe6a479b667a3c489faa655aca1190f79
Shreyash Maurya <168304543+shreyash-1908@users.noreply.github.com>
2026-02-23 22:43:08 +0530

    Create 1283_FindTheSmallestDivisorGivenAThreshold.java
    Added binary search on answer solution for LeetCode 1283 - Find the Smallest Divisor Given a Threshold

---
+++ Medium/1283_FindTheSmallestDivisorGivenAThreshold.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        
+        int left = 1;
+        int right = 0;
+        
+        // Upper bound = max element
+        for (int num : nums) {
+            right = Math.max(right, num);
+        }
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (canDivide(nums, threshold, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
+    private boolean canDivide(int[] nums, int threshold, int divisor) {
+        int sum = 0;
+        
+        for (int num : nums) {
+            sum += (num + divisor - 1) / divisor;  // ceil division
+        }
+        
+        return sum <= threshold;
+    }
+}
