65b4e6bcdab8e0e92490e02bb98b8a3ee74777d2
Shreyash Maurya <168304543+shreyash-1908@users.noreply.github.com>
2026-02-27 22:19:52 +0530

    Create 451_SortCharactersByFrequency.java
    Added HashMap + sorting solution for LeetCode 451 - Sort Characters By Frequency

---
+++ Medium/451_SortCharactersByFrequency.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String frequencySort(String s) {
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        List<Character> chars = new ArrayList<>(map.keySet());
+        
+        Collections.sort(chars, (a, b) -> map.get(b) - map.get(a));
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (char c : chars) {
+            int freq = map.get(c);
+            for (int i = 0; i < freq; i++) {
+                result.append(c);
+            }
+        }
+        
+        return result.toString();
+    }
+}
