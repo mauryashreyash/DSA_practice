f6734852b565b704846aa15c51524b1ec2f65c0f
Shreyash Maurya <168304543+mauryashreyash@users.noreply.github.com>
2026-08-12 13:48:01 +0530

    Right-Angled Number Pyramid

---
+++ Striver_A2Z/01_Basics/Patterns/03_Right_Angled_Number_pyramid.java
@@ -0,0 +1,39 @@
+// Pattern - 3: Right-Angled Number Pyramid
+// Problem Statement: Given an integer N, print the following pattern : 
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+class pattern3{
+    public static void pattern3(int N) {
+        for ( int i = 0 ; i< N; i++){
+            for (int j = 1; j<=i+1 ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+    }
+        public static void main(String[] args ){
+            int N = 5;
+            pattern3(N);
+        
+    }
+}
+
+/*
+Complexity
+
+There are:1 + 2 + 3 + ... + N
+
+number prints.That is:N(N + 1) / 2
+
+Therefore:
+Time: O(N²)
+
+Space: O(1) 
+
+ */
