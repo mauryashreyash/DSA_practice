0ee112dfb221152cb86926c7436f06a0fe32d41c
Shreyash Maurya <168304543+mauryashreyash@users.noreply.github.com>
2026-08-20 18:18:32 +0530

    Time: 8 ms (98.14%), Space: 48.4 MB (48.92%) - LeetHub

---
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int aliceTotal = 0;
+        int bobTotal = 0;
+
+        for (int candy : aliceSizes) {
+            aliceTotal += candy;
+        }
+
+        for (int candy : bobSizes) {
+            bobTotal += candy;
+        }
+
+        int diff = (aliceTotal - bobTotal) / 2;
+
+        Set<Integer> bob = new HashSet<>();
+
+        for (int candy : bobSizes) {
+            bob.add(candy);
+        }
+
+        for (int candy : aliceSizes) {
+            int need = candy - diff;
+
+            if (bob.contains(need)) {
+                return new int[]{candy, need};
+            }
+        }
+
+        return new int[9];
+    }
+}
