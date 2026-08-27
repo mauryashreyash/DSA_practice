5204a35864ad9f4918674615ae61081d776d903f
Shreyash Maurya <168304543+mauryashreyash@users.noreply.github.com>
2026-08-27 23:44:41 +0530

    Time: 0 ms (100%), Space: 46.6 MB (50.69%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.java
@@ -0,0 +1,76 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return build(grid, 0, 0, grid.length);
+    }
+
+    private Node build(int[][] grid, int row, int col, int size) {
+        int value = grid[row][col];
+        boolean same = true;
+
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (grid[i][j] != value) {
+                    same = false;
+                    break;
+                }
+            }
+
+            if (!same) {
+                break;
+            }
+        }
+
+        if (same) {
+            return new Node(value == 1, true);
+        }
+
+        int half = size / 2;
+
+        Node topLeft = build(grid, row, col, half);
+        Node topRight = build(grid, row, col + half, half);
+        Node bottomLeft = build(grid, row + half, col, half);
+        Node bottomRight = build(grid, row + half, col + half, half);
+
+        return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+}
